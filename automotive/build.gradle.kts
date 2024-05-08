3e4eb544f6f3fc9df1684239ce61af4f40fae48b
PatrikEdelenji <patrik.edelenji@gmail.com>
2024-05-08 22:17:27 +0200

    minor fixes regarding project files, added firebase dependencies

--- automotive/build.gradle.kts
+++ automotive/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -41,12 +42,13 @@ android {
 
 dependencies {
 
-    val fragment_version = "1.6.2"
+    val fragment_version = "1.7.0"
 
-    implementation("androidx.core:core-ktx:1.10.1")
+    implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("androidx.fragment:fragment-ktx:$fragment_version")
+    implementation(platform("com.google.firebase:firebase-bom:33.0.0"))
 }
